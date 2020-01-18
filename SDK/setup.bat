ab43137432634f5494bfdb69d581dc5af4aaf1b8
Glordim <anthony.tuffraud@gmail.com>
2020-01-18 12:23:16 +0100

    x64 and compilation

--- SDK/setup.bat
+++ SDK/setup.bat
@@ -29,7 +29,7 @@ if not exist "./Build/SDL" (
 	echo Create SDK Build Dir
 	mkdir "./Build/SDL"
 )
-cmake -B./Build/SDL ./SDL
+cmake -A x64 -B./Build/SDL ./SDL
 if %errorlevel% neq 0 (
 	echo Fail
 	pause
@@ -42,7 +42,7 @@ if not exist "./Build/glm" (
 	echo Create glm Build Dir
 	mkdir "./Build/glm"
 )
-cmake -B./Build/glm ./glm
+cmake -A x64 -B./Build/glm ./glm
 if %errorlevel% neq 0 (
 	echo Fail
 	pause
@@ -55,7 +55,7 @@ if not exist "./Build/SPIRV-CROSS" (
 	echo Create SPIRV-CROSS Build Dir
 	mkdir "./Build/SPIRV-CROSS"
 )
-cmake -B./Build/SPIRV-CROSS ./SPIRV-CROSS
+cmake -A x64 -B./Build/SPIRV-CROSS ./SPIRV-CROSS
 if %errorlevel% neq 0 (
 	echo Fail
 	pause
@@ -68,7 +68,7 @@ if not exist "./Build/tinyobjloader" (
 	echo Create tinyobjloader Build Dir
 	mkdir "./Build/tinyobjloader"
 )
-cmake -B./Build/tinyobjloader ./tinyobjloader
+cmake -A x64 -B./Build/tinyobjloader ./tinyobjloader
 if %errorlevel% neq 0 (
 	echo Fail
 	pause
@@ -87,7 +87,59 @@ if %errorlevel% neq 0 (
 
 if not exist "./Build/PhysX" (
 	echo Create SymLink to PhysX Build Dir
-	mklink /J /D %CD%\Build\PhysX %CD%\PhysX\physx\compiler\vc15win64
+	mklink /J %CD%\Build\PhysX %CD%\PhysX\physx\compiler\vc15win64
+)
+echo Done
+echo *******
+
+echo ====================
+
+::Build projects
+echo Build projects...
+
+echo *******
+echo SDL...
+cmake --build ./Build/SDL --config Release
+if %errorlevel% neq 0 (
+	echo Fail
+	pause
+	exit 1
+)
+echo Done
+echo *******
+echo glm...
+cmake --build ./Build/glm --config Release
+if %errorlevel% neq 0 (
+	echo Fail
+	pause
+	exit 1
+)
+echo Done
+echo *******
+echo SPIRV-CROSS...
+cmake --build ./Build/SPIRV-CROSS --config Release
+if %errorlevel% neq 0 (
+	echo Fail
+	pause
+	exit 1
+)
+echo Done
+echo *******
+echo tinyobjloader...
+cmake --build ./Build/tinyobjloader --config Release
+if %errorlevel% neq 0 (
+	echo Fail
+	pause
+	exit 1
+)
+echo Done
+echo *******
+echo PhysX...
+cmake --build ./Build/PhysX --config Release
+if %errorlevel% neq 0 (
+	echo Fail
+	pause
+	exit 1
 )
 echo Done
 echo *******
