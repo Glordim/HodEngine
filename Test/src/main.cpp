2a330bc9275fb2d2327ac1a330892af57a74f18c
Glordim <anthony.tuffraud@gmail.com>
2021-02-25 22:30:54 +0100

    Clean

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -11,8 +11,6 @@
 
 using namespace HOD;
 
-//InputListener inputListener;
-
 //-----------------------------------------------------------------------------
 //! @brief		
 //-----------------------------------------------------------------------------
@@ -21,18 +19,24 @@ int main(int argc, char** argv)
 	MyApplication app;
 
 	if (app.Init() == false)
+	{
 		return false;
+	}
 
 	int selectedMonitor = 0;
 
 	// For Debug
 	if (APPLICATION::ScreenHelper::GetMonitorCount() > 1)
+	{
 		selectedMonitor = 1;
+	}
 
 	APPLICATION::Resolution nativeResolution;
 
 	if (APPLICATION::ScreenHelper::GetNativeResoltion(&nativeResolution, selectedMonitor) == false)
+	{
 		return 1;
+	}
 
 	APPLICATION::VideoSettings videoSettings;
 	videoSettings.monitor = selectedMonitor;
@@ -40,10 +44,14 @@ int main(int argc, char** argv)
 	videoSettings.fullscreenMode = APPLICATION::FullscreenMode::Windowed;
 
 	if (app.CreateWindowAndContext("HodEngine", videoSettings) == false)
+	{
 		return 1;
+	}
 
 	if (app.Run() == false)
+	{
 		return 1;
+	}
 
 	return 0;
 }
