b67cbed8f82db3213239a1a62bdab34ccf8f36c2
Glordim <anthony.tuffraud@gmail.com>
2021-08-01 01:08:08 +0200

    [Editor] new style

--- HodEngine-Editor/Src/main.cpp
+++ HodEngine-Editor/Src/main.cpp
@@ -5,17 +5,32 @@
 #include "Contents/ContentFactory.h"
 
 #include <QApplication>
+#include "Style/DarkStyle.h"
+#include "Style/framelesswindow/framelesswindow.h"
 
 int main(int argc, char** argv)
 {
 	QApplication application(argc, argv);
+	QApplication::setStyle(new DarkStyle);
 
 	Project::CreateInstance();
 	ContentDataBase::CreateInstance();
 	ContentFactory::Init();
 
-	MainWindow mainWindow;
-	mainWindow.show();
+	FramelessWindow framelessWindow;
+
+	MainWindow* mainWindow = new MainWindow();
+	mainWindow->setWindowIcon(application.style()->standardIcon(QStyle::SP_DesktopIcon));
+
+	QObject::connect(mainWindow, &MainWindow::windowTitleChanged, &framelessWindow, &FramelessWindow::setWindowTitle);
+	QObject::connect(mainWindow, &MainWindow::windowIconChanged, &framelessWindow, &FramelessWindow::setWindowIcon);
+
+	framelessWindow.setContent(mainWindow);
+	framelessWindow.show();
+
+	mainWindow->Refresh();
+	framelessWindow.setWindowTitle(mainWindow->windowTitle());
+	framelessWindow.setWindowIcon(mainWindow->windowIcon());
 
 	return application.exec();
 }
