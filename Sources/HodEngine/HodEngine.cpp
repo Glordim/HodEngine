8c00c7085332fdefcb35bb210d1d572d533fb449
Glordim <anthony.tuffraud@gmail.com>
2024-06-15 23:29:14 +0200

    Fix Cmake with XCode

---
+++ Sources/HodEngine/HodEngine.cpp
@@ -0,0 +1,14 @@
+/*
+Dummy file
+Require by some native build system to use OBJ lib
+From Cmake doc :
+From Some native build systems (such as Xcode) may not like targets that have only object files,
+so consider adding at least one real source file to any target that references $<TARGET_OBJECTS:objlib>.
+*/
+
+namespace hod
+{
+    void HodEngineDummy()
+    {
+    }
+}
