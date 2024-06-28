5a5bb653bc21e39ddb2367800f5fbf840b15bcef
Glordim <anthony.tuffraud@gmail.com>
2024-06-28 10:12:03 +0200

    Split Dialog per Platform

---
+++ Sources/HodEngine/Application/PlatformDialog.linux.cpp
@@ -0,0 +1,42 @@
+#include <HodEngine/Application/PlatformDialog.hpp>
+
+namespace hod::application::dialog
+{
+	/// @brief 
+	/// @return 
+	std::filesystem::path GetOpenFileDialog()
+	{
+		char buffer[4096] = { '\0' };
+
+		// TODO
+
+		return buffer;
+	}
+
+	/// @brief 
+	/// @return 
+	std::filesystem::path GetSaveFileDialog(const std::string_view& typeName, const std::string_view& typeExtension, const std::filesystem::path& initialFolder)
+	{
+		char buffer[4096] = { '\0' };
+
+		// TODO
+
+		return buffer;
+	}
+
+	/// @brief 
+	/// @return 
+	std::filesystem::path GetFolderDialog()
+	{
+		// TODO
+
+		return std::filesystem::path();
+	}
+
+	/// @brief 
+	/// @param path 
+	void OpenExplorerAtPath(const std::filesystem::path& path)
+	{
+		// TODO
+	}
+}
---
+++ Sources/HodEngine/Application/PlatformDialog.windows.cpp
@@ -0,0 +1,111 @@
+#include <HodEngine/Application/PlatformDialog.hpp>
+
+#include <Windows.h>
+#include <shlobj_core.h>
+
+namespace hod::application::dialog
+{
+	/// @brief 
+	/// @return 
+	std::filesystem::path GetOpenFileDialog()
+	{
+		char buffer[4096] = { '\0' };
+
+		OPENFILENAMEA openFileName;
+		ZeroMemory(&openFileName, sizeof(openFileName));
+		openFileName.lStructSize = sizeof(openFileName);
+		openFileName.hwndOwner = NULL;
+		openFileName.lpstrFile = buffer;
+		openFileName.nMaxFile = sizeof(buffer);
+		openFileName.lpstrFilter = "Hod Project\0*.hod\0";
+		openFileName.nFilterIndex = 1;
+		openFileName.lpstrFileTitle = NULL;
+		openFileName.nMaxFileTitle = 0;
+		openFileName.lpstrInitialDir = NULL;
+		openFileName.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+		GetOpenFileNameA(&openFileName);
+
+		return buffer;
+	}
+
+	/// @brief 
+	/// @return 
+	std::filesystem::path GetSaveFileDialog(const std::string_view& typeName, const std::string_view& typeExtension, const std::filesystem::path& initialFolder)
+	{
+		char buffer[4096] = { '\0' };
+
+		strcpy(buffer, "EditMe.");
+		strcat(buffer, typeExtension.data());
+
+		char filter[4096] = { '\0' };
+		strcpy(filter, typeName.data());
+		strcpy(filter + typeName.size() + 1, typeExtension.data());
+
+		std::string initialFolderPath = initialFolder.string();
+
+		OPENFILENAMEA openFileName;
+		ZeroMemory(&openFileName, sizeof(openFileName));
+		openFileName.lStructSize = sizeof(openFileName);
+		openFileName.hwndOwner = NULL;
+		openFileName.lpstrFile = buffer;
+		openFileName.nMaxFile = sizeof(buffer);
+		openFileName.lpstrFilter = filter;
+		openFileName.nFilterIndex = 1;
+		openFileName.lpstrFileTitle = NULL;
+		openFileName.nMaxFileTitle = 0;
+		if (initialFolder.empty() == true)
+		{
+			openFileName.lpstrInitialDir = NULL;
+		}
+		else
+		{
+			openFileName.lpstrInitialDir = initialFolderPath.c_str();
+		}
+		openFileName.Flags = 0;//OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+		GetSaveFileNameA(&openFileName);
+
+		return buffer;
+	}
+
+	/// @brief 
+	/// @return 
+	std::filesystem::path GetFolderDialog()
+	{
+		char buffer[MAX_PATH] = { '\0' }; 
+		BROWSEINFOA browseInfo;
+		browseInfo.hwndOwner = NULL;
+		browseInfo.pidlRoot = NULL;
+		browseInfo.pszDisplayName = buffer;
+		browseInfo.lpszTitle = "Select a folder";
+		browseInfo.ulFlags = BIF_NEWDIALOGSTYLE;
+		browseInfo.lpfn = NULL;
+		browseInfo.lParam = 0;
+		browseInfo.iImage = -1;
+		LPITEMIDLIST itemIdLists = SHBrowseForFolderA(&browseInfo);
+		if (itemIdLists != NULL)
+		{
+			CHAR dirPaths[MAX_PATH];
+			SHGetPathFromIDListA(itemIdLists, dirPaths);
+			CoTaskMemFree(itemIdLists);
+			return dirPaths;
+		}
+
+		return std::filesystem::path();
+	}
+
+	/// @brief 
+	/// @param path 
+	void OpenExplorerAtPath(const std::filesystem::path& path)
+	{
+		STARTUPINFOA startupInfo;
+		ZeroMemory(&startupInfo, sizeof(startupInfo));
+		startupInfo.cb = sizeof(startupInfo);
+
+		PROCESS_INFORMATION processInformation;
+		ZeroMemory(&processInformation, sizeof(processInformation));
+
+		CreateProcessA(NULL, (LPSTR)(std::string("explorer.exe ") + path.string()).c_str(), NULL, NULL, FALSE, 0, NULL, NULL, &startupInfo, &processInformation);
+	}
+}
