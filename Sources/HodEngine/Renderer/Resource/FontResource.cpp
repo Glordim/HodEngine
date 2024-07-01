aec6366ae795fa468d8f46cbc2283a0a326ba1cc
Glordim <anthony.tuffraud@gmail.com>
2024-07-01 10:27:25 +0200

    FontResource generate an ugly atlas

--- Sources/HodEngine/Renderer/Resource/FontResource.cpp
+++ Sources/HodEngine/Renderer/Resource/FontResource.cpp
@@ -52,6 +52,17 @@ namespace hod::renderer
 		//FT_Outline_Transform(&slot->outline, &matrix);
 	}
 
+	unsigned int nextPowerOf2(unsigned int n)
+	{
+		if ((n & (n - 1)) == 0) return n;
+
+		unsigned int power = 1;
+		while (power < n) {
+			power <<= 1;
+		}
+		return power;
+	}
+
 	/// @brief 
 	/// @param document 
 	/// @param stream 
@@ -100,74 +111,131 @@ namespace hod::renderer
 		FT_Face ftFace;
 		error = FT_New_Memory_Face(ftLibrary, data, dataSize, 0, &ftFace);
 
-		delete[] data;
-
 		if (error != 0)
 		{
+			delete[] data;
 			FT_Done_FreeType(ftLibrary);
 			OUTPUT_ERROR("FreeType::FT_New_Memory_Face %s", FT_Error_String(error));
 			return false;
 		}
-    	error = FT_Set_Pixel_Sizes(ftFace, 0, 64);
+
+		error = FT_Select_Charmap(ftFace, FT_ENCODING_UNICODE);
+		if (error != 0)
+		{
+			FT_Done_Face(ftFace);
+			delete[] data;
+			FT_Done_FreeType(ftLibrary);
+			OUTPUT_ERROR("FreeType::FT_Select_Charmap %s", FT_Error_String(error));
+			return false;
+		}
+
+    	error = FT_Set_Pixel_Sizes(ftFace, 0, 48);
 		if (error != 0)
 		{
 			OUTPUT_ERROR("FreeType::FT_Set_Pixel_Sizes %s", FT_Error_String(error));
 			FT_Done_Face(ftFace);
+			delete[] data;
 			FT_Done_FreeType(ftLibrary);
 			return false;
 		}
 
-		error = FT_Load_Char(ftFace, 'A', FT_LOAD_DEFAULT);
-		if (error != 0)
+		struct CharacterData
 		{
-			OUTPUT_ERROR("FreeType::FT_Load_Char %s", FT_Error_String(error));
-			FT_Done_Face(ftFace);
-			FT_Done_FreeType(ftLibrary);
-			return false;
-		}
+			uint8_t* pixels = nullptr;
+			uint32_t width = 0;
+			uint32_t height = 0;
+		};
 
-		// Render the glyph to a bitmap
-		error = FT_Render_Glyph(ftFace->glyph, FT_RENDER_MODE_NORMAL);
-		if (error != 0)
+		std::vector<CharacterData> characterDatas;
+		characterDatas.reserve(128);
+
+		for (uint8_t c = 0; c < 128; ++c)
 		{
-			OUTPUT_ERROR("FreeType::FT_Render_Glyph %s\n", FT_Error_String(error));
-			FT_Done_Face(ftFace);
-			FT_Done_FreeType(ftLibrary);
-			return false;
-		}
-
-		// Access the bitmap
-		FT_Bitmap ftBitmap = ftFace->glyph->bitmap;
-
-		Texture::CreateInfo createInfo;
-
-		uint8_t* pixels = new uint8_t[ftBitmap.rows * ftBitmap.width * 4];
-		for (uint32_t y = 0; y < ftBitmap.rows; y++)
-		{
-			for (uint32_t x = 0; x < ftBitmap.width; x++)
+			FT_UInt charIndex = FT_Get_Char_Index(ftFace, c);
+			if (charIndex == 0)
 			{
-				unsigned char pixel = ftBitmap.buffer[y * ftBitmap.pitch + x];
-				pixels[y * ftBitmap.width * 4 + x * 4] = 255;
-				pixels[y * ftBitmap.width * 4 + x * 4 + 1] = 255;
-				pixels[y * ftBitmap.width * 4 + x * 4 + 2] = 255;
-				pixels[y * ftBitmap.width * 4 + x * 4 + 3] = pixel;
+				continue;
+			}
+
+			error = FT_Load_Glyph(ftFace, charIndex, FT_LOAD_DEFAULT);
+			if (error != 0)
+			{
+				OUTPUT_ERROR("FreeType::FT_Load_Glyph %s", FT_Error_String(error));
+				FT_Done_Face(ftFace);
+				delete[] data;
+				FT_Done_FreeType(ftLibrary);
+				return false;
+			}
+
+			// Render the glyph to a bitmap
+			error = FT_Render_Glyph(ftFace->glyph, FT_RENDER_MODE_NORMAL);
+			if (error != 0)
+			{
+				OUTPUT_ERROR("FreeType::FT_Render_Glyph %s\n", FT_Error_String(error));
+				FT_Done_Face(ftFace);
+				delete[] data;
+				FT_Done_FreeType(ftLibrary);
+				return false;
+			}
+
+			FT_Bitmap ftBitmap = ftFace->glyph->bitmap;
+			if (ftBitmap.buffer != nullptr && ftBitmap.rows > 0 && ftBitmap.width > 0)
+			{
+				CharacterData characterData;
+				characterData.pixels = new uint8_t[ftBitmap.rows * ftBitmap.width * 4](0);
+				characterData.width = ftBitmap.width;
+				characterData.height = ftBitmap.rows;
+				for (uint32_t y = 0; y < ftBitmap.rows; y++)
+				{
+					for (uint32_t x = 0; x < ftBitmap.width; x++)
+					{
+						unsigned char pixel = ftBitmap.buffer[y * ftBitmap.pitch + x];
+						characterData.pixels[y * ftBitmap.width * 4 + x * 4] = 255;
+						characterData.pixels[y * ftBitmap.width * 4 + x * 4 + 1] = 255;
+						characterData.pixels[y * ftBitmap.width * 4 + x * 4 + 2] = 255;
+						characterData.pixels[y * ftBitmap.width * 4 + x * 4 + 3] = pixel;
+					}
+				}
+
+				characterDatas.push_back(characterData);
 			}
 		}
 
 		FT_Done_Face(ftFace);
+		delete[] data;
 		FT_Done_FreeType(ftLibrary);
 
-		_texture = Renderer::GetInstance()->CreateTexture();
-		if (_texture->BuildBuffer(ftBitmap.width, ftBitmap.rows, (unsigned char*)pixels, createInfo) == false) // todo BuildBuffer doesn't take void* ?
+		uint32_t characterByLine = (uint32_t)std::sqrt(characterDatas.size()) + 1;
+		uint32_t atlasWidth = nextPowerOf2(characterByLine * 48);
+		uint8_t* atlas = new uint8_t[atlasWidth * atlasWidth * 4](0);
+		for (uint32_t characteIndex = 0; characteIndex < characterDatas.size(); ++characteIndex)
 		{
-			delete[] pixels;
+			const CharacterData& characterData = characterDatas[characteIndex];
 
+			uint32_t lineOffset = (characteIndex / characterByLine) * atlasWidth * 4 * 48;
+
+			for (uint32_t y = 0; y < characterData.height; ++y)
+			{
+				std::memcpy(atlas + lineOffset + (characteIndex % characterByLine) * 48 * 4 + y * atlasWidth * 4, characterData.pixels + y * characterData.width * 4, 4 * characterData.width);
+			}
+		}
+		for (const CharacterData& characterData : characterDatas)
+		{
+			delete[] characterData.pixels;
+		}
+
+		Texture::CreateInfo createInfo;
+		_texture = Renderer::GetInstance()->CreateTexture();
+		if (_texture->BuildBuffer(atlasWidth, atlasWidth, (unsigned char*)atlas, createInfo) == false) // todo BuildBuffer doesn't take void* ?
+		{
+			delete[] atlas;
+			
 			delete _texture;
 			_texture = nullptr;
 
 			return false;
 		}
-		delete[] pixels;
+		delete[] atlas;
 		
 		return true;
 	}
