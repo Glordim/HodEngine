dd627f94a8c207be697a0bf741d2481a06fcdb49
Glordim <anthony.tuffraud@gmail.com>
2024-08-19 23:44:15 +0200

    Core: CreateProcess, add stdout stderr redirection (win32)

--- Sources/HodEngine/Core/Process/Process.windows.cpp
+++ Sources/HodEngine/Core/Process/Process.windows.cpp
@@ -1,5 +1,6 @@
 #include "HodEngine/Core/pch.hpp"
 #include "HodEngine/Core/Process/Process.hpp"
+#include "HodEngine/Core/Output/OutputService.hpp"
 
 #include <Windows.h>
 
@@ -11,11 +12,32 @@ namespace hod
 	/// @return 
 	bool Process::Create(const std::string_view& program, const std::string_view& argument, bool detach)
 	{
-		STARTUPINFO si;
-		PROCESS_INFORMATION pi;
+		SECURITY_ATTRIBUTES saAttr;
+		saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+		saAttr.bInheritHandle = TRUE;
+		saAttr.lpSecurityDescriptor = NULL;
 
+		HANDLE hStdOutRead, hStdOutWrite;
+		HANDLE hStdErrRead, hStdErrWrite;
+
+		if (!CreatePipe(&hStdOutRead, &hStdOutWrite, &saAttr, 0) || !CreatePipe(&hStdErrRead, &hStdErrWrite, &saAttr, 0))
+		{
+			OUTPUT_ERROR("Failed to create pipes ({})", GetLastError());
+			return false;
+		}
+
+		SetHandleInformation(hStdOutRead, HANDLE_FLAG_INHERIT, 0);
+		SetHandleInformation(hStdErrRead, HANDLE_FLAG_INHERIT, 0);
+
+		STARTUPINFOA si;
 		ZeroMemory(&si, sizeof(si));
 		si.cb = sizeof(si);
+		si.dwFlags = STARTF_USESTDHANDLES;
+		si.hStdOutput = hStdOutWrite;
+		si.hStdError = hStdErrWrite;
+		si.hStdInput = NULL;
+
+		PROCESS_INFORMATION pi;
 		ZeroMemory(&pi, sizeof(pi));
 
 		CHAR resolvedProgram[MAX_PATH];
@@ -25,21 +47,87 @@ namespace hod
 		commandLine += " ";
 		commandLine += argument;
 
-		if (!CreateProcessA(NULL, (char*)commandLine.c_str(), NULL, NULL, FALSE, detach ? DETACHED_PROCESS : 0, NULL, NULL, &si, &pi)) 
+		if (!CreateProcessA(NULL, (char*)commandLine.c_str(), NULL, NULL, TRUE, CREATE_NO_WINDOW | (detach ? DETACHED_PROCESS : 0), NULL, NULL, &si, &pi)) 
 		{
-			//std::cerr << std::format("CreateProcess failed ({})\n", GetLastError());
+			OUTPUT_ERROR("Failed to create process ({})", GetLastError());
+			CloseHandle(hStdOutRead);
+			CloseHandle(hStdOutWrite);
+			CloseHandle(hStdErrRead);
+			CloseHandle(hStdErrWrite);
 			return false;
 		}
 
-		if (detach == false)
+		CloseHandle(hStdOutWrite);
+		CloseHandle(hStdErrWrite);
+
+		bool result;
+		if (detach == true)
 		{
-			// Wait until child process exits.
-			WaitForSingleObject( pi.hProcess, INFINITE );
+			result = true;
+		}
+		else
+		{
+			char buffer[4096];
+			DWORD bytesRead;
+			BOOL success;
+			DWORD exitCode;
+
+			while (true)
+			{
+				// Vérifier si le processus est terminé
+				if (WaitForSingleObject(pi.hProcess, 0) == WAIT_OBJECT_0)
+				{
+					break;
+				}
+
+				// Lire les données de stdout en temps réel
+				while (PeekNamedPipe(hStdOutRead, NULL, 0, NULL, &bytesRead, NULL) && bytesRead > 0)
+				{
+					success = ReadFile(hStdOutRead, buffer, sizeof(buffer) - 1, &bytesRead, NULL);
+					if (success && bytesRead > 0)
+					{
+						buffer[bytesRead] = '\0';
+						OUTPUT_MESSAGE(buffer);
+					}
+				}
+
+				// Lire les données de stderr en temps réel
+				while (PeekNamedPipe(hStdErrRead, NULL, 0, NULL, &bytesRead, NULL) && bytesRead > 0)
+				{
+					success = ReadFile(hStdErrRead, buffer, sizeof(buffer) - 1, &bytesRead, NULL);
+					if (success && bytesRead > 0)
+					{
+						buffer[bytesRead] = '\0';
+						OUTPUT_ERROR(buffer);
+					}
+				}
+
+				Sleep(10);
+			}
+
+			// Assurez-vous de lire toute sortie restante après la fin du processus
+			while (ReadFile(hStdOutRead, buffer, sizeof(buffer) - 1, &bytesRead, NULL) && bytesRead > 0)
+			{
+				buffer[bytesRead] = '\0';
+				OUTPUT_MESSAGE(buffer);
+			}
+
+			while (ReadFile(hStdErrRead, buffer, sizeof(buffer) - 1, &bytesRead, NULL) && bytesRead > 0)
+			{
+				buffer[bytesRead] = '\0';
+				OUTPUT_ERROR(buffer);
+			}
+
+			// Récupérer le code de sortie du processus
+			GetExitCodeProcess(pi.hProcess, &exitCode);
+			result = (exitCode == EXIT_SUCCESS);
 		}
 
+		CloseHandle(hStdOutRead);
+		CloseHandle(hStdErrRead);
 		CloseHandle(pi.hProcess);
 		CloseHandle(pi.hThread);
 
-		return true;
+		return result;
 	}
 }
