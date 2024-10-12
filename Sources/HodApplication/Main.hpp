4b14cabe58fa443a6c47b8a0a83d8fa64d19b264
Glordim <anthony.tuffraud@gmail.com>
2024-10-12 12:30:18 +0200

    Fix compile

--- Sources/HodApplication/Main.hpp
+++ Sources/HodApplication/Main.hpp
@@ -2,14 +2,20 @@
 
 #include <HodEngine/HodEngine.hpp>
 
-#if defined(HOD_APP_EXPORT)
-	#define HOD_APP_EXPORT HOD_EXPORT
+#if defined(PLATFORM_ANDROID)
+	#if defined(HOD_APP_EXPORT)
+		#define HOD_APP_EXPORT HOD_EXPORT
+	#else
+		#define HOD_APP_EXPORT HOD_IMPORT
+	#endif
 #else
-	#define HOD_APP_EXPORT HOD_IMPORT
+	#define HOD_APP_EXPORT
 #endif
 
 extern "C"
 {
 	HOD_APP_EXPORT int HodMain(int argc, char** argv);
+#if defined(PLATFORM_ANDROID)
 	HOD_APP_EXPORT void android_main(struct android_app* app);
+#endif
 }
