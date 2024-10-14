f5681e5a3bba356e9dd26e49aa0abe4195d8895a
Glordim <anthony.tuffraud@gmail.com>
2024-10-14 10:48:26 +0200

    Linux: add missing implementation

---
+++ Sources/HodEngine/Core/FileSystemWatcher/FileSystemWatcher.linux.cpp
@@ -0,0 +1,23 @@
+#include "HodEngine/Core/Pch.hpp"
+#include "HodEngine/Core/FileSystemWatcher/FileSystemWatcher.hpp"
+
+namespace hod
+{
+	/// @brief 
+	/// @param path 
+	/// @return 
+	bool FileSystemWatcher::InternalInit()
+	{
+		return true;
+	}
+
+	/// @brief 
+	void FileSystemWatcher::Cleanup()
+	{
+	}
+
+	/// @brief 
+	void FileSystemWatcher::Update()
+	{
+	}
+}
