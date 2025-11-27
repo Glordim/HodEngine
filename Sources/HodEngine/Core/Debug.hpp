2c2a057f19ef4a7ab29d1f7899ac9473b6e4c1b0
Glordim <anthony.tuffraud@gmail.com>
2025-11-27 23:24:09 +0100

    Fix Break for GCC

--- Sources/HodEngine/Core/Debug.hpp
+++ Sources/HodEngine/Core/Debug.hpp
@@ -4,11 +4,19 @@
 namespace hod
 {
 	// Macro ensures the breakpoint triggers in caller code
-#if defined(PLATFORM_WINDOWS)
-	#define Break() __debugbreak();
-#else
-	#define Break() __builtin_debugtrap();
-#endif
+	#if defined(_MSC_VER) || (defined(_WIN32) && defined(__clang__))
+		#define Break() __debugbreak()
+	#elif defined(__clang__)
+		#define Break() __builtin_debugtrap()
+	#else
+		#if defined(__x86_64__)
+			#define Break() __asm__("int3")
+		#elif defined(__aarch64__)
+			#define Break() __asm__("brk #0")
+		#else
+			#error
+		#endif
+	#endif
 
 	HOD_CORE_API bool IsDebuggerAttached();
 }
