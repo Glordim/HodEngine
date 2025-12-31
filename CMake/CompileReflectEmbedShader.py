42a1178571e1483fba6d024b8dacf7c47d9abfc0
Glordim <anthony.tuffraud@gmail.com>
2025-12-31 01:16:54 +0100

    Renderer: Add Slang reflection in EmbedShader (preapre SpirvCross removal and DX PSO creation)

--- CMake/CompileReflectEmbedShader.py
+++ CMake/CompileReflectEmbedShader.py
@@ -30,13 +30,12 @@ def compile_shader(slangc, input_file, target_api, profile, shader_stage, output
 	if profile is not None:
 		args.extend(["-profile", profile])
 
-	# todo
-	#if reflection_json is not None:
-	#	args.extend(["-r", reflection_json])
+	if reflection_json is not None:
+		args.extend(["-reflection-json", reflection_json])
 
 	subprocess.run(args, check=True)
 
-def embed_to_cpp_hpp(identifier, binary_path, text_path, out_cpp, out_hpp, tpl_cpp, tpl_hpp):
+def embed_to_cpp_hpp(identifier, binary_path, text_path, reflection_path, out_cpp, out_hpp, tpl_cpp, tpl_hpp):
 
 	def bytes_to_cpp_array(data: bytes, line_length=16) -> str:
 		parts = [f'0x{b:02x}' for b in data]
@@ -45,36 +44,56 @@ def embed_to_cpp_hpp(identifier, binary_path, text_path, out_cpp, out_hpp, tpl_c
 			lines.append(', '.join(parts[i:i+line_length]))
 		return ',\n\t'.join(lines)
 
+	def bytes_to_cpp_text(data: bytes) -> str:
+		if not data:
+			return '""'
+
+		text = data.decode('utf-8', errors='replace')
+		
+		lines = text.splitlines()
+		escaped_lines = []
+		
+		for line in lines:
+			safe_line = line.replace('\\', '\\\\').replace('"', '\\"')
+			escaped_lines.append(f'"{safe_line}\\n"')
+
+		return '\n\t'.join(escaped_lines)
+
 	def make_cpp_identifier(identifier: str) -> str:
 		base = os.path.basename(identifier)
 		name = os.path.splitext(base)[0]
 		return ''.join(c if c.isalnum() else '_' for c in name)
 
-	def generate_from_template(output_path, template_path, varname, array_content, comment):
+	def generate_from_template(output_path, template_path, varname, bytecode_content, reflection_content, comment):
 		with open(template_path, 'r') as tpl:
 			template = tpl.read()
 		output = (
 			template
 			.replace('@ESCAPED_FILE_NAME@', varname)
-			.replace('@ESCAPED_FILE_CONTENTS@', array_content)
+			.replace('@BYTECODE_CONTENTS@', bytecode_content)
 			.replace('@COMMENT@', comment)
+			.replace('@REFLECTION_CONTENTS@', reflection_content)
 		)
 		with open(output_path, 'w') as f:
 			f.write(output)
 
 	with open(binary_path, 'rb') as f:
-		data = f.read()
+		binaryData = f.read()
+
+	with open(reflection_path, 'rb') as f:
+		reflectionData = f.read()
 
 	comment = ''
-	if os.path.exists(text_path):
-		with open(text_path, 'r') as f:
+	if os.path.exists(reflection_path):
+		with open(reflection_path, 'r') as f:
 			comment = f.read().strip()
 
-	array_content = bytes_to_cpp_array(data)
+	bytecode_content = bytes_to_cpp_array(binaryData)
+	reflection_content = bytes_to_cpp_text(reflectionData)
 	varname = make_cpp_identifier(identifier)
 
-	generate_from_template(out_cpp, tpl_cpp, varname, array_content, comment)
-	generate_from_template(out_hpp, tpl_hpp, varname, array_content, comment)
+	generate_from_template(out_cpp, tpl_cpp, varname, bytecode_content, reflection_content, comment)
+	generate_from_template(out_hpp, tpl_hpp, varname, bytecode_content, reflection_content, comment)
 
 def main():
 	if len(sys.argv) != 11:
@@ -118,13 +137,16 @@ def main():
 	text_output = os.path.join(tmp_dir, f"{base_name}_{shader_stage}.{text_ext}")
 	reflection_output = os.path.join(tmp_dir, f"{base_name}_{shader_stage}.json")
 
+	print(f"Shader IL: {binary_output}")
+	print(f"Shader Text: {text_output}")
+	print(f"Shader Reflection: {reflection_output}")
+
 	compile_shader(slangc, input_file, bytecode_target, profile, shader_stage.lower(), binary_output, reflection_output)
 	if config == "Debug":
 		compile_shader(slangc, input_file, text_target, profile, shader_stage.lower(), text_output)
 
-	#todo embed reflection
 	identifier = f"{base_name}_{shader_stage}"
-	embed_to_cpp_hpp(identifier, binary_output, text_output, out_cpp, out_hpp, tpl_cpp, tpl_hpp)
+	embed_to_cpp_hpp(identifier, binary_output, text_output, reflection_output, out_cpp, out_hpp, tpl_cpp, tpl_hpp)
 
 if __name__ == "__main__":
 	main()
