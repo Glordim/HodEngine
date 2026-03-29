a0f8b4569e85ef91b2482aa971d4326b9dd9017a
Glordim <anthony.tuffraud@gmail.com>
2026-03-29 19:37:51 +0200

    Core support static build

--- Sources/HodEngine/Core/Export.hpp
+++ Sources/HodEngine/Core/Export.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <HodEngine/HodEngine.hpp>
 
-#ifdef HOD_CORE_EXPORT
+#if defined(HOD_CORE_STATIC)
+	#define HOD_CORE_API 
+#elif defined(HOD_CORE_EXPORT)
 	#define HOD_CORE_API HOD_EXPORT
 #else
 	#define HOD_CORE_API HOD_IMPORT
