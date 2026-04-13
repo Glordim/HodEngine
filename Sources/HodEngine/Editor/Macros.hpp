a037155b7dec3a5491a389610e6437bedb3424c9
Glordim <anthony.tuffraud@gmail.com>
2026-04-13 15:21:25 +0200

    Editor: Clean old macro

--- Sources/HodEngine/Editor/Macros.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "HodEngine/Editor/Trait/ReflectionTraitComponentCustomEditor.hpp"
-
-#define ADD_CUSTOM_EDITOR(Class) AddTrait<ReflectionTraitComponentCustomEditor>(DefaultAllocator::GetInstance().New<Class>())
