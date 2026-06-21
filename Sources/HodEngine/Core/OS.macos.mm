5afefc494d5770cfefa011c3b834d7932f086dc2
Glordim <anthony.tuffraud@gmail.com>
2026-06-21 12:41:09 +0200

    MacOs: Implement Clipboard function

--- Sources/HodEngine/Core/OS.macos.mm
+++ Sources/HodEngine/Core/OS.macos.mm
@@ -18,11 +18,31 @@ namespace hod::inline core
 
 	bool OS::ReadClipboard(String& value)
 	{
-		return false;
+		@autoreleasepool {
+			NSPasteboard* pasteboard = [NSPasteboard generalPasteboard];
+			NSString* nsString = [pasteboard stringForType:NSPasteboardTypeString];
+			if (nsString == nil)
+				return false;
+
+			const char* utf8 = [nsString UTF8String];
+			if (utf8 == nullptr)
+				return false;
+
+			value = utf8;
+		}
+		return true;
 	}
 
 	bool OS::WriteClipboard(const std::string_view& value)
 	{
-		return false;
+		@autoreleasepool {
+			NSPasteboard* pasteboard = [NSPasteboard generalPasteboard];
+			[pasteboard clearContents];
+			NSString* nsString = [[NSString alloc] initWithBytes:value.data() length:value.size() encoding:NSUTF8StringEncoding];
+			if (nsString == nil)
+				return false;
+
+			return [pasteboard setString:nsString forType:NSPasteboardTypeString] == YES;
+		}
 	}
 }
