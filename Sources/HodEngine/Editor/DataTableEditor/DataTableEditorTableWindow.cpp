28ab4f6bf1252fa60838f2b10c97e13b0124c042
Glordim <anthony.tuffraud@gmail.com>
2026-07-16 21:13:17 +0200

    DataTable: Editor (wip)

--- Sources/HodEngine/Editor/DataTableEditor/DataTableEditorTableWindow.cpp
+++ Sources/HodEngine/Editor/DataTableEditor/DataTableEditorTableWindow.cpp
@@ -1,13 +1,19 @@
 #include "HodEngine/Editor/Pch.hpp"
-#include "HodEngine/Editor/DataTableEditor/DataTableEditorTableWindow.hpp"
+#include "HodEngine/Core/Reflection/ReflectionDescriptor.hpp"
+#include "HodEngine/Core/Reflection/ReflectionProperty.hpp"
 #include "HodEngine/Editor/DataTableEditor/DataTableEditorTab.hpp"
+#include "HodEngine/Editor/DataTableEditor/DataTableEditorTableWindow.hpp"
 
+#include "HodEngine/Editor/EditorReflectedObject.hpp"
+#include "HodEngine/Editor/PropertyDrawer.hpp"
+#include "HodEngine/Game/DataStructFactory.hpp"
 #include "HodEngine/Game/DataTableContainer.hpp"
 
 #include <HodEngine/ImGui/DearImGui/imgui.h>
 #include <HodEngine/ImGui/Font/IconsMaterialDesignIcons.h>
 
 #include <algorithm>
+#include <cstdint>
 #include <vector>
 
 namespace hod::inline editor
@@ -29,7 +35,9 @@ namespace hod::inline editor
 	void DataTableEditorTableWindow::DrawContent()
 	{
 		DataTableEditorTab* editorTab = GetOwner<DataTableEditorTab>();
-		DataTableContainer&  dataTableContainer = editorTab->GetDataTableContainer();
+		DataTableContainer& dataTableContainer = editorTab->GetDataTableContainer();
+
+		bool changed = false;
 
 		if (ImGui::Button(ICON_MDI_PLUS " Add Row") == true)
 		{
@@ -42,7 +50,7 @@ namespace hod::inline editor
 			}
 			dataTableContainer.AddRow(newKey);
 			editorTab->SetRowSelection(newKey);
-			editorTab->MarkAsDirty();
+			changed = true;
 		}
 
 		std::vector<const String*> keys;
@@ -53,48 +61,68 @@ namespace hod::inline editor
 		}
 		std::sort(keys.begin(), keys.end(), [](const String* left, const String* right) { return *left < *right; });
 
-		if (ImGui::BeginTable("DataTableRows", 2, ImGuiTableFlags_Sortable | ImGuiTableFlags_RowBg | ImGuiTableFlags_Borders) == true)
+		ReflectionDescriptor* reflectionDescriptor = DataStructFactory::GetInstance()->FindReflectionDescriptor(dataTableContainer.GetRowType());
+		if (reflectionDescriptor != nullptr)
 		{
-			ImGui::TableSetupColumn("Key", ImGuiTableColumnFlags_WidthStretch);
-			ImGui::TableSetupColumn("##Delete", ImGuiTableColumnFlags_WidthFixed | ImGuiTableColumnFlags_NoSort, 24.0f);
-			ImGui::TableHeadersRow();
-
-			const String& selectedKey = editorTab->GetRowSelectionKey();
-			const String* keyToDelete = nullptr;
-
-			for (const String* key : keys)
+			uint32_t columnCount = 1 + reflectionDescriptor->GetProperties().Size();
+			if (ImGui::BeginTable("DataTableRows", columnCount, ImGuiTableFlags_Sortable | ImGuiTableFlags_RowBg | ImGuiTableFlags_Borders) == true)
 			{
-				ImGui::PushID(key);
-				ImGui::TableNextRow();
-
-				ImGui::TableNextColumn();
-				bool selected = (*key == selectedKey);
-				if (ImGui::Selectable(key->CStr(), selected, ImGuiSelectableFlags_SpanAllColumns) == true)
+				ImGui::TableSetupColumn("Key", ImGuiTableColumnFlags_WidthStretch);
+				for (ReflectionProperty* reflectionProperty : reflectionDescriptor->GetProperties())
 				{
-					editorTab->SetRowSelection(*key);
+					ImGui::TableSetupColumn(reflectionProperty->GetDisplayName().CStr(), ImGuiTableColumnFlags_WidthStretch);
+				}
+				ImGui::TableHeadersRow();
+
+				const String& selectedKey = editorTab->GetRowSelectionKey();
+				const String* keyToDelete = nullptr;
+
+				DataTableContainer& dataTable = editorTab->GetDataTableContainer();
+
+				for (const auto& pair : dataTable.GetRows())
+				{
+					ImGui::PushID(pair.first.CStr());
+					ImGui::TableNextRow();
+
+					ImGui::TableNextColumn();
+					bool selected = (pair.first == selectedKey);
+					if (ImGui::Selectable(pair.first.CStr(), selected, ImGuiSelectableFlags_SpanAllColumns) == true)
+					{
+						editorTab->SetRowSelection(pair.first);
+					}
+
+					EditorReflectedObject reflectedObject(pair.second, reflectionDescriptor, nullptr, this);
+					for (ReflectionProperty* reflectionProperty : reflectionDescriptor->GetProperties())
+					{
+						ImGui::TableNextColumn();
+						EditorReflectedProperty* reflectedProperty = reflectedObject.FindProperty(reflectionProperty->GetName());
+						if (reflectedProperty != nullptr)
+						{
+							changed |= PropertyDrawer::DrawProperty(*reflectedProperty);
+						}
+					}
+
+					ImGui::PopID();
 				}
 
-				ImGui::TableNextColumn();
-				if (ImGui::SmallButton(ICON_MDI_DELETE) == true)
+				if (keyToDelete != nullptr)
 				{
-					keyToDelete = key;
+					String deletedKey = *keyToDelete;
+					dataTableContainer.RemoveRow(deletedKey);
+					if (editorTab->GetRowSelectionKey() == deletedKey)
+					{
+						editorTab->SetRowSelection("");
+					}
+					editorTab->MarkAsDirty();
 				}
 
-				ImGui::PopID();
+				ImGui::EndTable();
 			}
+		}
 
-			if (keyToDelete != nullptr)
-			{
-				String deletedKey = *keyToDelete;
-				dataTableContainer.RemoveRow(deletedKey);
-				if (editorTab->GetRowSelectionKey() == deletedKey)
-				{
-					editorTab->SetRowSelection("");
-				}
-				editorTab->MarkAsDirty();
-			}
-
-			ImGui::EndTable();
+		if (changed)
+		{
+			editorTab->MarkAsDirty();
 		}
 	}
 }
